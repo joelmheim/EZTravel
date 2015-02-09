b26f854d46f22922df8ae32d5d2a7d0554f00a7b
Jørn Ølmheim <joe@statoil.com>
2015-02-09 10:50:15 +0100

    Added some serverside auth code with passport

--- server/app.js
+++ server/app.js
@@ -1,13 +1,60 @@
 var express = require('express');
+var passport = require('passport');
+var SamlStrategy = require('passport-azure-ad').SamlStrategy;
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var methodOverride = require('method-override');
 var receipts = require('./routes/receipts');
 var trips = require('./routes/trips');
 
 var app = express();
+
+var config = {
+  // required options
+  identityMetadata: 'https://login.windows.net/3aa4a235-b6e2-48d5-9195-7fcf05b459b0/federationmetadata/2007-06/federationmetadata.xml',
+  loginCallback: 'https://eztravel-joelmheim.c9.io/login/callback/',
+  issuer: 'https://eztravel-joelmheim.c9.io/'
+};
+
+// array to hold logged in users. State of the art.
+var users = [];
+
+var findByEmail = function(email, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.email === email) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+};
+
+// Keep a reference to the saml Strategy as we will need it for an eventual logout
+var samlStrategy = new SamlStrategy(config, function(profile, done) {
+  if (!profile.email) {
+    return done(new Error("No email found"), null);
+  }
+  // asynchronous verification, for effect. Wow!
+  process.nextTick(function () {
+    findByEmail(profile.email, function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        // "Auto-registration"
+        users.push(profile);
+        return done(null, profile);
+      }
+      return done(null, user);
+    });
+  });
+});
+
+passport.use(samlStrategy);
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -15,9 +62,40 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(methodOverride());
+app.use(session({ secret: 'dis-hackathon-2015' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// To enzure authentication, put this on all request handlers
+var ensureAuthenticated = function(req, res, next) {
+  if (req.isAuthenticated()) {
+    console.log("Is authenticated - number of users: " + users.length);
+    return next();
+  }
+  res.redirect('/login');
+};
 
 app.use('/api/receipts', receipts);
 app.use('/api/trips', trips);
+
+app.get('/api/user', ensureAuthenticated, function(req, res){
+  res.json({ user: req.user });
+});
+
+app.get('/login',
+  passport.authenticate('saml', { failureRedirect: '/', failureFlash: true }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+app.post('/login/callback',
+  passport.authenticate('saml', { failureRedirect: '/', failureFlash: true }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
 
 if (app.get('env') === 'development') {
   console.log('Server running in ' + app.get('env') + ' mode.');
@@ -48,5 +126,20 @@ if (app.get('env') == 'production') {
     });
   });
 }
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+// //   the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+  done(null, user.email);
+});
+
+passport.deserializeUser(function(id, done) {
+  findByEmail(id, function (err, user) {
+    done(err, user);
+  });
+});
 
 module.exports = app;
