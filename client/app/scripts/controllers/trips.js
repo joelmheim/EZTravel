54182e0474b596355ec0750bd9ae591538c39e96
Kristian Flikka <kristian.flikka@gmail.com>
2015-02-10 10:56:17 +0100

    Added formatting of timestamps

--- client/app/scripts/controllers/trips.js
+++ client/app/scripts/controllers/trips.js
@@ -9,6 +9,14 @@
  */
 angular.module('clientApp').
 	controller('TripsCtrl', function ($scope, $http) {
-    	$http.get("/api/trips").success(function(response) {$scope.trips = response;});
+    	$http.get("/api/trips").success(function(response) {
+    		$scope.trips = response;
+    		$scope.trips.forEach(function(trip) {
+    			trip.start = Date.parse(trip.start);
+    			trip.end = Date.parse(trip.end);
+    			trip.created_at = Date.parse(trip.created_at);
+    		}
+    		);
+    	});
     });
 
